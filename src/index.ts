5ed54e1a6cfbb2f4040aa6f49c42892e5ee49b1e
Abdullah Bin Amir <75297261+AbdullahBinAmir@users.noreply.github.com>
2024-09-23 23:07:55 +0500

    backend

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import express,{Application,Request,Response} from 'express'
+import "dotenv/config"
+const app:Application = express()
+const PORT = process.env.PORT || 7000
+import path from 'path'
+import { fileURLToPath } from 'url'
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+
+app.set("view engine","ejs")
+app.set("views",path.resolve(__dirname,"./views"))
+
+app.get("/",(req:Request,res:Response)=>{
+    res.render("welcome")
+})
+
+app.listen(PORT,()=>console.log(`Server is running on PORT ${PORT}`))
