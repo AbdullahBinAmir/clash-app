2c0dca6c1a8f7244c84627b2fa0d314a3f84cca5
Abdullah Bin Amir <75297261+AbdullahBinAmir@users.noreply.github.com>
2024-09-29 11:52:20 +0500

    emails: nodemailer added

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,6 +4,8 @@ const app:Application = express()
 const PORT = process.env.PORT || 7000
 import path from 'path'
 import { fileURLToPath } from 'url'
+import ejs from "ejs"
+import { sendEmail } from './config/config.js'
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 app.use(express.json())
@@ -12,8 +14,10 @@ app.use(express.urlencoded({extended:false}))
 app.set("view engine","ejs")
 app.set("views",path.resolve(__dirname,"./views"))
 
-app.get("/",(req:Request,res:Response)=>{
-    res.render("welcome")
+app.get("/",async(req:Request,res:Response)=>{
+    const html = await ejs.renderFile(__dirname+`/views/emails/welcome.ejs`,{name:"ok"})
+    await sendEmail("ok@gmail.com","test mail",html)
+    res.json({msg:"welcome"})
 })
 
 app.listen(PORT,()=>console.log(`Server is running on PORT ${PORT}`))
